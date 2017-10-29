b8ac5c8610f19be169802a47e330331035e2c186
lamartire <lamartire@gmail.com>
2017-10-29 18:10:15 +0300

    Initial

---
+++ src/templates/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="{{ __NAME__ | kebab }}">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: '{{ __NAME__ }}',
+
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style>
+  .{{ __NAME__ | kebab }} {
+    display: block;
+  }
+</style>
